828e1b1079f44ac616b4fb3f8b8237e9c531d892
(s) Callum Edwards 1 <callum.edwards-9@students.plymouth.ac.uk>
2020-11-13 14:43:54 +0000

    Mongo DB integration, currently writes index to user table

--- QuizPals/server.js
+++ QuizPals/server.js
@@ -2,7 +2,42 @@
 var http = require('http');
 var fs = require('fs');
 var url = require("url");
+var mongo = require("mongodb");
+//var nodemailer = require('nodemailer'); //might be needed, sends emails, not in use for now
 //var datetime = require('./Business/datetime');
+
+
+
+//region MongoDB
+const MongoClient = require('mongodb').MongoClient;
+//const uri = "mongodb+srv://dbUser:COMP3006pw@cluster0.8a4if.mongodb.net/<database name>?retryWrites=true&w=majority";
+const uri = "mongodb+srv://dbUser:COMP3006pw@cluster0.8a4if.mongodb.net/QuizPals?retryWrites=true&w=majority";
+const client = new MongoClient(uri, { useNewUrlParser: true });
+client.connect(err => {
+    // perform actions on the collection object
+    if (err) throw err;
+
+    //connects to the database
+    var dbo = client.db("QuizPals");
+
+    //Inserts a record to the db
+    var myobj = { name: "Callum Edwards", User: "CalEdwards" };
+    dbo.collection("User").insertOne(myobj, function (err, res) {
+        if (err) throw err;
+        console.log("1 document inserted");
+        client.close();
+    });
+
+    //Lists all the users in the user table
+    dbo.collection("User").find({}).toArray(function (err, result) {
+        if (err) throw err;
+        console.log(result);
+        client.close();
+    });
+    //client.close();
+});
+//end region
+
 
 const hostname = '127.0.0.1';
 var port = process.env.PORT || 9000;
@@ -25,3 +60,7 @@ http.createServer(function (req, res) {
     //res.write('Hello World\n')
     //res.end();
 }).listen(port, hostname);
+
+
+
+
