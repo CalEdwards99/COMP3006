ce5a8bb716f4941b92463e08469a03b10a29933b
(s) Callum Edwards 1 <callum.edwards-9@students.plymouth.ac.uk>
2020-12-28 15:11:57 +0000

    Started on MEAN stack

--- QuizPals/server.js
+++ QuizPals/server.js
@@ -2,13 +2,19 @@
 var http = require('http');
 var fs = require('fs');
 var url = require("url");
+
+//let path = require("path");
 var mongo = require("mongodb");
+const mongoose = require("mongoose");
+var express = require("express");
+let db = require("./Database/db");
 //var nodemailer = require('nodemailer'); //might be needed, sends emails, not in use for now
 //var datetime = require('./Business/datetime');
 
 
 
 //region MongoDB
+
 const MongoClient = require('mongodb').MongoClient;
 //const uri = "mongodb+srv://dbUser:COMP3006pw@cluster0.8a4if.mongodb.net/<database name>?retryWrites=true&w=majority";
 const uri = "mongodb+srv://dbUser:COMP3006pw@cluster0.8a4if.mongodb.net/QuizPals?retryWrites=true&w=majority";
@@ -21,24 +27,93 @@ client.connect(err => {
     var dbo = client.db("QuizPals");
 
     //Inserts a record to the db
-    var myobj = { name: "Callum Edwards", User: "CalEdwards" };
-    dbo.collection("User").insertOne(myobj, function (err, res) {
-        if (err) throw err;
-        console.log("1 document inserted");
-        client.close();
-    });
+    //var myobj = { name: "Callum Edwards", User: "CalEdwards" };
+    //dbo.collection("User").insertOne(myobj, function (err, res) {
+    //    if (err) throw err;
+    //    console.log("1 document inserted");
+    //    client.close();
+    //});
 
     //Lists all the users in the user table
-    dbo.collection("User").find({}).toArray(function (err, result) {
-        if (err) throw err;
-        console.log(result);
-        client.close();
-    });
+    //dbo.collection("User").find({}).toArray(function (err, result) {
+    //    if (err) throw err;
+    //    console.log(result);
+    //    client.close();
+    //});
     //client.close();
 });
 //end region
 
 
+//region database
+
+//connecting to the database
+mongoose.connect(uri, { useUnifiedTopology: true, useNewUrlParser: true });
+
+var dbo2 = mongoose.connection;
+
+dbo2.on('error', console.error.bind(console, 'connection error:'));
+dbo2.once('open', function () {
+    console.log("connection successful")
+    //mongoose.connection.db.listCollections().toArray(function (err, names) {
+    //    console.log(names); // [{ name: 'dbname.myCollection' }]
+    //});
+});
+
+var User = mongoose.model('user')
+
+var Schema = mongoose.Schema;
+//var userSchema = new Schema({
+//    name: String,
+//    User: String
+//});
+
+//const userSchema = mongoose.model('User', Schema({
+//    name: String,
+//    User: String
+//}));
+
+//Create model===================================================
+//var UserModel = dbo2.model('User', userSchema);
+
+Console.log(User.find())
+
+//userSchema.find();
+
+//const user = mongoose.model('User', userSchema);
+
+//user.find(function (err, users) {
+//    if (err) return console.error(err);
+//    console.log(users);
+//})
+
+
+
+//dbo2.collection("User").find({}).toArray(function (err, result) {
+//    if (err) throw err;
+//    console.log(result);
+//});
+
+async function listAllUsers(request, response) {
+    let users = await db.listAllUsers();
+    let data = { users: users };
+    response.render("users", data);
+}
+
+//let app = express();
+
+//Setup the app to use EJS templates.
+//app.set("web", path.join(__dirname, "web"));
+//app.set("view engine", "ejs");
+
+//app.get("/Web/JoinQuiz", listAllUsers);
+
+//app.listen()
+
+//end region
+
+
+//region server
 const hostname = '127.0.0.1';
 var port = process.env.PORT || 9000;
 
@@ -60,6 +135,8 @@ http.createServer(function (req, res) {
     //res.write('Hello World\n')
     //res.end();
 }).listen(port, hostname);
+
+//end region
 
 
 
